6e67ed7df8ef8e97205368c3a4d17b1821e86dbc
Thi Phuc Thinh Huynh <ph.thinh905@gmail.com>
2023-09-13 16:52:05 -0700

    Modify problem 01

--- error-handling.js
+++ error-handling.js
@@ -1,26 +1,67 @@
 // 1.
+
 function sum(array) {
-  let sum = 0;
+let sum = 0;
+
+try {
+  // console.log(nonexistentVariable); // To try differernt error which is ReferenceError
   for (let i = 0; i < array.length; i++) {
     sum += array[i];
   }
-  return sum;
 }
+
+catch (error) {
+  if (error instanceof TypeError) {
+    console.log(error.message);
+  }
+  else {
+    throw error;
+  }
+}
+
+return sum;
+}
+
 
 let res = sum(null);
 console.log(res);
 
+
 // 2.
 // tests
-sayName("Alex");
-sayName(1);
-// Your code here
+
+function sayName(name) {
+  if (typeof name === "string") {
+    console.log(name);
+  }
+  else {
+    throw new TypeError("Invalid name! Must be a string!");
+  }
+}
+
+try {
+    sayName("Alex");
+    sayName(1);
+}
+catch(e) {
+    console.log(e.message);
+}
+
+
+
 
 // 3.
+
 function greet(greeting) {
   if (!greeting) {
     throw new Error("There was no greeting given.");
   }
 
   console.log(greeting);
+}
+
+try {
+  greet();
+} catch (error) {
+  console.log("Hello World!");
 }
